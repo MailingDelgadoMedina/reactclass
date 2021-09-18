f000bf83e07e97b0e34a8c524d8b0599204b7470
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2021-09-18 02:08:01 -0400

    Passing props example using a product. It's passing the info from App.js to Product.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Product from './Product';
 
 function App() {
   return (
     <div className="App">
-    
-
+    <Product name='doll' description='toy' price={60.99} />
+{/* */}
     
     </div>
   );
